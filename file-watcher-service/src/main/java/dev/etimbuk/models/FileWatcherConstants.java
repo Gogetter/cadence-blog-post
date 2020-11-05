3f679b622cdd92d865faca57159e02610037dff6
gogetterm <euedofia@gmail.com>
2020-11-05 18:26:09 +0000

    removed unused variable

--- file-watcher-service/src/main/java/dev/etimbuk/models/FileWatcherConstants.java
+++ file-watcher-service/src/main/java/dev/etimbuk/models/FileWatcherConstants.java
@@ -6,5 +6,4 @@ import lombok.NoArgsConstructor;
 public final class FileWatcherConstants {
     public static final String workflowDomain = "file-watcher";
     public static final String fileUploadTask = "file-upload-task";
-    public static final String notificationTask = "file-notification-task";
 }
