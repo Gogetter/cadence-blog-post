6b0d541d6d37f224716f1ff08ea5d5f87b8635a4
gogetterm <euedofia@gmail.com>
2020-11-05 18:58:15 +0000

    file-notification-service intial commit

---
+++ file-notification-service/src/main/java/dev/etimbuk/filenotificationservice/FileNotificationServiceApplication.java
@@ -0,0 +1,13 @@
+package dev.etimbuk.filenotificationservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FileNotificationServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FileNotificationServiceApplication.class, args);
+    }
+
+}
---
+++ file-notification-service/src/test/java/dev/etimbuk/filenotificationservice/FileNotificationServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.etimbuk.filenotificationservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FileNotificationServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
