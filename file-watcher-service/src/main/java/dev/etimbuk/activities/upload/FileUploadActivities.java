963c3ad3f06a098833c08c5fb63e23c82db3d724
gogetterm <euedofia@gmail.com>
2020-11-05 18:27:54 +0000

    removed unused import

--- file-watcher-service/src/main/java/dev/etimbuk/activities/upload/FileUploadActivities.java
+++ file-watcher-service/src/main/java/dev/etimbuk/activities/upload/FileUploadActivities.java
@@ -4,8 +4,6 @@ import com.uber.cadence.activity.ActivityMethod;
 import dev.etimbuk.models.UploadResponse;
 import dev.etimbuk.models.WorkflowData;
 
-import java.nio.file.Path;
-
 public interface FileUploadActivities {
     @ActivityMethod(scheduleToCloseTimeoutSeconds = 100)
     UploadResponse uploadFile(WorkflowData workflowData);
