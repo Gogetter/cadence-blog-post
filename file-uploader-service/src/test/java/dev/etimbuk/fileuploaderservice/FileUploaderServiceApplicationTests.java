fd06142e0c39a86f30bfa07cd2c9d8299ad00ff0
gogetterm <euedofia@gmail.com>
2020-11-05 18:50:53 +0000

    file-upload-service initial commit

---
+++ file-uploader-service/src/test/java/dev/etimbuk/fileuploaderservice/FileUploaderServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.etimbuk.fileuploaderservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FileUploaderServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
