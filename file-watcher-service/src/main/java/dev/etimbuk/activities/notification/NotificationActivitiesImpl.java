a8b006fcfa52d6e5a6e8815737c01fe788e66503
gogetterm <euedofia@gmail.com>
2020-11-05 18:36:30 +0000

    semantic update

--- file-watcher-service/src/main/java/dev/etimbuk/activities/notification/NotificationActivitiesImpl.java
+++ file-watcher-service/src/main/java/dev/etimbuk/activities/notification/NotificationActivitiesImpl.java
@@ -1,5 +1,6 @@
 package dev.etimbuk.activities.notification;
 
+import com.uber.cadence.activity.Activity;
 import dev.etimbuk.models.UploadResponse;
 import lombok.extern.slf4j.Slf4j;
 
@@ -7,6 +8,6 @@ import lombok.extern.slf4j.Slf4j;
 public class NotificationActivitiesImpl implements NotificationActivities {
     @Override
     public void sendNotification(final UploadResponse uploadResponse) {
-      log.info("Notification sent");
+        log.info("Notification sent");
     }
 }
