4aa47a4261d2c17160797d8fb1c80c3cad00dbca
gogetterm <euedofia@gmail.com>
2020-11-05 18:31:22 +0000

    updated task list (queue name)

--- file-watcher-service/src/main/java/dev/etimbuk/models/FileWatcherConstants.java
+++ file-watcher-service/src/main/java/dev/etimbuk/models/FileWatcherConstants.java
@@ -5,5 +5,5 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 public final class FileWatcherConstants {
     public static final String workflowDomain = "file-watcher";
-    public static final String fileUploadTask = "file-upload-task";
+    public static final String fileUploadTask = "file-processing-task-list";
 }
